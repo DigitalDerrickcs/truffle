a8e9ccbfe917b9f7a06da4a1bd31f27f4a91148c
g. nicholas d'andrea <gnidan@trufflesuite.com>
2021-01-06 23:05:53 -0500

    Move connect/serve logic to meta system
    - Define `forAttachAndSchema` in src/meta/interface
    - Use this function in unified `forDefinitions`
    - Make `src/connect` and `src/server` into simple passthroughs

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,46 +1,9 @@
 import { logger } from "@truffle/db/logger";
 const debug = logger("db:db");
 
-import gql from "graphql-tag";
-import { DocumentNode, ExecutionResult, execute } from "graphql";
-
-import type TruffleConfig from "@truffle/config";
-
-import { Db } from "./meta";
+import { Db, forDefinitions } from "./meta";
 export { Db }; // rather than force src/index from touching meta
 
-import { Workspace } from "./resources";
-import { schema } from "./schema";
-import { attach } from "./workspace";
+import { definitions } from "./resources";
 
-export const connect = (config: TruffleConfig): Db => {
-  const workspace: Workspace = attach({
-    workingDirectory: config.working_directory,
-    adapter: (config.db || {}).adapter
-  });
-
-  return {
-    async execute(
-      request: DocumentNode | string,
-      variables: any = {}
-    ): Promise<ExecutionResult> {
-      const response = await execute(
-        schema,
-        typeof request === "string"
-          ? gql`
-              ${request}
-            `
-          : request,
-        null,
-        { workspace },
-        variables
-      );
-
-      if (response.errors) {
-        debug("errors %o", response.errors);
-      }
-
-      return response;
-    }
-  };
-};
+export const { connect } = forDefinitions(definitions);
